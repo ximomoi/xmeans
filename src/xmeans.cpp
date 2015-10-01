a215e9a91853b92bca94b7d8d58a23ce0845bd01
ximo112 <ryo.shimo112@gmail.com>
2015-10-01 20:07:39 +0900

    test kmeans

--- src/xmeans.cpp
+++ src/xmeans.cpp
@@ -1,22 +1,14 @@
 #include <ros/ros.h>
-#include <stdio.h>
-#include <math.h>
-#include <sstream>
 #include <iostream>
-#include <complex>
-#include <Eigen/Core>
-#include <Eigen/Dense>
-#include <Eigen/Geometry>
-#include <Eigen/LU>
-#include <stdlib.h>
-#include <time.h>
-//#include <nag.h>
-//#include <nagg05.h>
+#include <math.h>
 #include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/PointCloud.h>
-#include <tf/transform_broadcaster.h>
-#include <tf/transform_listener.h>
-#define obstacle_max 100
+#include <Eigen/Dense>
+#include <time.h>
+#include <sstream>
+#include <complex>
+#include <stdlib.h>
+#define obstacle_max 720
 
 using namespace Eigen;
 
@@ -24,186 +16,192 @@ class Xmeans{
 public:
   Xmeans(){
     scan_sub = nh.subscribe("/scan", 10, &Xmeans::scanCallBack, this);
-
     obstacle_cluster_pub = nh.advertise<sensor_msgs::PointCloud>("/obstacle_cluster", 1000);
+
+    //objects_info(x, y, k_distance, cluster_number, likelihood)
+    objects_info = MatrixXf::Zero(5, obstacle_max);
+    laser = VectorXf::Zero(obstacle_max);
+
+    cluster.points.resize(obstacle_max);
+
+    obstacle_last_num = obstacle_max;
 
     ros::Rate loop_rate(10);
   }
 
   void scanCallBack(const sensor_msgs::LaserScan::ConstPtr& scan){
-    //objects_info(x, y, k_dif, k_number, likelihood)
-    objects_info = MatrixXf::Zero(6, 100);
-
-    float deg = -45, rad;
-    for(int i = 0; i < obstacle_max; i++){
-      rad = deg * M_PI / 180;
-      objects_info(1, i+1) = scan->ranges[i] * cos(rad);
-      objects_info(2, i+1) = scan->ranges[i] * sin(rad);
+    int h = 0, i;
+    float deg = 0, rad;
+    obstacle_last_num = obstacle_max;
+    ROS_INFO("-------------hogehoge---------------");
+    for(i = 0; i < obstacle_max; i++){
+      laser(i) = scan->ranges[i];
+      if(laser(i) > 29){
+        obstacle_last_num += -1;
+      }else{
+        rad = deg * M_PI / 180;
+        objects_info(0, h) = scan->ranges[i] * cos(rad);
+        objects_info(1, h) = scan->ranges[i] * sin(rad);
+        h += 1;
+      }
       deg += 0.25;
     }
   }
-/*
-  //BIC_k
-  void BIC1(){
-    int q = 2 * (2 + 3) / 2;
-    double bic1 = -2 * log(k_likelihood) + q * log(obstacle_max);
-  }
 
-  //BIC_k+1
-  void BIC2(){
+  void kmeans(int k, int kakunin){
+    int h, i, j, count, error = 0;
+    float deg_random, rad_random, range_random, distance, cluster_old_x[k], cluster_old_y[k], x_add, y_add, center_coordinate_x = 0, center_coordinate_y = 0, center_distance, center_distance_max;
+    //cluster_info(x, y, number, 未使用, cov_det_add)
+    cluster_info = MatrixXf::Zero(5, k);
 
-    //0.5 / 確率密度関数
-    double alpha = 0.5 / (exp(- pow(beta, 2) / 2) / sqrt(2 * M_PI));
-
-    double bic2 = -2 * (cluster_max * log(alpha) + log(likelihood(j)) + log(likelihood(j+1))) + (2 * q) * log(obstacle_max);
-  }
-*/
-
-  //kmeans
-  void kmeans(int k){
-    int i, j, num, decision = 0, count;
-    float cluster_dif, x_add, y_add;
-    //cluster_info(x, y, number, calc, cov_det_add)
-    MatrixXf cluster_info = MatrixXf::Zero(5, k);
-    //適当にクラスタの座標を設定,urgの範囲が30m
-    for(j = 1; j <= k; j++){
-      cluster_info(1, j) = (((float)rand() / RAND_MAX) * 60) - 30;
-      cluster_info(2, j) = (((float)rand() / RAND_MAX) * 60) - 30;
+    for(i = 0; i < obstacle_max; i++){
+      if(laser(i) == 0){
+        error += 1;
+      }
     }
-    while(true){
-      //各ノードと各クラスタの距離を計算し、ノードの属するクラスタを決める
-      for(i = 1; i <= obstacle_max; i++){
-        for(j = 1; j <= k; j++){
-          cluster_info(4, j) = hypotf(objects_info(1, i) - cluster_info(1, j), objects_info(2, i) - cluster_info(2, j));
-          if(j == 1){
-            cluster_dif = cluster_info(4, j);
-            num = j;
-          }else{
-            if(cluster_info(4, j - 1) > cluster_info(4, j)){
-              cluster_dif = cluster_info(4, j);
-              num = j;
+    if(error == obstacle_max){
+      ROS_ERROR("Laser scan value is zero");
+    }else if(obstacle_last_num == 0){
+      ROS_ERROR("There is no dynamic obstacle");
+    }else{
+      //取得した障害物の座標を元にランダムクラスタを配置する
+      for(h = 0; h < obstacle_last_num; h++){
+        center_coordinate_x += objects_info(0, h);
+        center_coordinate_y += objects_info(1, h);
+      }
+      center_coordinate_x = center_coordinate_x / obstacle_last_num;
+      center_coordinate_y = center_coordinate_y / obstacle_last_num;
+      for(h = 0; h < obstacle_last_num; h++){
+        center_distance = hypotf(objects_info(0, h) - center_coordinate_x, objects_info(1, h) - center_coordinate_y);
+        if(h == 0){
+          center_distance_max = center_distance;
+        }else if(center_distance_max < center_distance){
+          center_distance_max = center_distance;
+        }
+      }
+
+      srand(time(NULL));
+      for(j = 0; j < k; j++){
+        deg_random = (float)rand() / RAND_MAX * 360;
+        rad_random = deg_random * M_PI / 180;
+        range_random = (float)rand() / RAND_MAX * center_distance_max;
+        cluster_info(0, j) = range_random * cos(rad_random) + center_coordinate_x;
+        cluster_info(1, j) = range_random * sin(rad_random) + center_coordinate_y;
+        cluster_info(2, j) = j;
+
+        ROS_INFO("rand_clus_x:%f, rand_clus_y:%f", cluster_info(0, j), cluster_info(1, j));
+      }
+
+      int syuusoku = 0;
+      while(true){
+        int decision = 0;
+        //クラスタの割り当て
+        for(h = 0; h < obstacle_last_num; h++){
+          for(j = 0; j < k; j++){
+            distance = hypotf(objects_info(0, h) - cluster_info(0, j), objects_info(1, h) - cluster_info(1, j));
+            //
+//            ROS_INFO("k:%d, distance:%f", j, distance);
+            if(j == 0){
+              objects_info(2, h) = distance;
+              objects_info(3, h) = j;
+            }else if(objects_info(2, h) > distance){
+              objects_info(2, h) = distance;
+              objects_info(3, h) = j;
             }
           }
+//          ROS_INFO("select_k:%f", objects_info(3, h));
         }
-        //各クラスタと全ノードの距離が変化しているかどうか
-        if(objects_info(3, i) == cluster_dif && objects_info(4, i) == num){
-          decision += 1;
-        }else{
-          objects_info(3, i) = cluster_dif;
-          objects_info(4, i) = num;
+        //確認
+        ROS_INFO("1-----");
+        ROS_INFO("clus_x1:%f, clus_y1:%f", cluster_info(0, 0), cluster_info(1, 0));
+        ROS_INFO("clus_x2:%f, clus_y2:%f", cluster_info(0, 1), cluster_info(1, 1));
+        ROS_INFO("obje_rang180:%f, obje_rang360:%f, obje_rang540:%f", laser(180), laser(360), laser(540));
+        ROS_INFO("range_rand:%f, deg_rand:%f", range_random, deg_random);
+        ROS_INFO("------");
+
+        //クラスタの移動
+        for(j = 0; j < k; j++){
+          //
+          cluster_old_x[j] = cluster_info(0, j);
+          cluster_old_y[j] = cluster_info(1, j);
+
+          x_add = 0;
+          y_add = 0;
+          count = 0;
+          for(h = 0; h < obstacle_last_num; h++){
+            if(cluster_info(2, j) == objects_info(3, h)){
+              x_add += objects_info(0, h);
+              y_add += objects_info(1, h);
+              count += 1;
+            }
+          }
+          if(count == 0){
+            ROS_INFO("cluster No:%d does not move", j);
+          }else{
+            cluster_info(0, j) = x_add / count;
+            cluster_info(1, j) = y_add / count;
+          }
+          //確認
+          ROS_INFO("count:%d, k:%d", count, j);
         }
-      }
-      //各クラスタと全ノードとの距離が変化しなくなった場合
-      if(decision == obstacle_max){
-        break;
-      }
-      //決めたノードの重心へクラスタを移動
-      for(j = 1; j <= k; j++){
-        count = 0;
-        for(i = 1; i <= obstacle_max; i++){
-          if(cluster_info(3, j) == objects_info(4, i)){
-            x_add += objects_info(1, i);
-            y_add += objects_info(2, i);
-            count += 1;
+        //確認
+        ROS_INFO("2-----");
+        ROS_INFO("clus_old_x1:%f, clus_old_y1:%f", cluster_old_x[0], cluster_old_y[0]);
+        ROS_INFO("clus_old_x2:%f, clus_old_y2:%f", cluster_old_x[1], cluster_old_y[1]);
+        ROS_INFO("------");
+
+
+        //今回のクラスタ座標と前回のクラスタ座標を比較
+        for(j = 0; j < k; j++){
+          if(cluster_info(0, j) == cluster_old_x[j] && cluster_info(1, j) == cluster_old_y[j]){
+            decision += 1;
           }
         }
-        cluster_info(1, j) = x_add / count;
-        cluster_info(2, j) = y_add / count;
-      }
-    }
-    for(j = 1; j <= k; j++){
-      obstacle_cluster.points[j].x = cluster_info(1, j);
-      obstacle_cluster.points[j].y = cluster_info(2, j);
-    }
-    ROS_INFO("x:%f, y:%f \n", cluster_info(1, k), cluster_info(2, k));
-  }
-/*
-  //尤度計算
-  void Likelihood(){
-    int i, j, count;
-    //共分散行列
-    cov = Matrix2d::Zero;
-    for(j = 1; j <= k; j++){
-      count = 0;
-      for(i = 1; i <= obstacle_max; i++){
-        if(cluster_info(3, j) == objects_info(4, i)){
-          cov(1, 1) += pow(cluster_info(1, j) - objects_info(1, i), 2);
-          cov(2, 2) += pow(cluster_info(2, j) - objects_info(2, i), 2);
-          cov(1, 2) += (cluster_info(1, j) - objects_info(1, i)) * (cluster_info(2, j) - objects_info(2, i));
-          count += 1;
+        if(decision == k){
+          break;
         }
-      }
-      cov(1, 1) = cov(1, 1) / count;
-      cov(2, 2) = cov(2, 2) / count;
-      cov(1, 2) = cov(1, 2) / count;
-      cov(2, 1) = cov(1, 2);
+        //確認
+        ROS_INFO("3-----");
+        ROS_INFO("clus_x1:%f, clus_y1:%f", cluster_info(0, 0), cluster_info(1, 0));
+        ROS_INFO("clus_x2:%f, clus_y2:%f", cluster_info(0, 1), cluster_info(1, 1));
+        ROS_INFO("loop:%d, kaisuu:%d, last_num:%d", kakunin, syuusoku, obstacle_last_num);
+        ROS_INFO("------");
 
-      cluster_info(5, j) += cov.determinant();
+        syuusoku += 1;
 
-      //多変量正規分布
-      f = Vector1080d::Zero;
-      x = Vector2d::Zero;
-      ave = Vector2d::Zero;
-      ave(1) = cluster_info(1, j);
-      ave(2) = cluster_info(2, j);
-      for(i = 1; i <= obstacle_max; i++){
-        x(1) = i;
-        x(2) = i;
-        f(i) = 1 / ((double)sqrt(pow(2 * M_PI, 2) * cov.determinant()) * (double)exp(- (x - ave).transpose() * cov.inverse() * (x - ave) / 2);
       }
 
-      //尤度関数
-      likelihood(j) = f.sum();
+      for(j = 0; j < k; j++){
+        cluster.points[j].x = cluster_info(0, j);
+        cluster.points[j].y = cluster_info(1, j);
 
-      //確認用
-      ROS_INFO("likelihood%d:%f \n", j, likelihood(j));
+//        ROS_INFO("x:%f, y:%f, num:%d \n", cluster_info(0, j), cluster_info(1, j), j);
+      }
+      obstacle_cluster_pub.publish(cluster);
     }
-    ROS_INFO("---\n");
-    //double k_likelihood = likelihood.sum();
+    ROS_INFO("--------------finish");
   }
-*/
-/*
-  //BICの比較
-  void decision(){
-    if(bic1 < bic2){
-      k += 1;
-      return false;
-    }else{
-      return true;
-    }
-  }
-*/
-  //run
+
   void run(){
+    int kakunin = 0;
     while(ros::ok()){
-      int k = 1;
-//      do{
-//      for(i = 0; i < 2; i++){
-      kmeans(k);
-//        Likelihood();
-//        k += 1;
-//      }
-//        BIC();
-//        desicsion();
-//      }while(decision == true);
+      int k = 2;
+      kmeans(k, kakunin);
       ros::spinOnce();
-//      break;
+      kakunin += 1;
     }
   }
-
 private:
   ros::NodeHandle nh;
   ros::Subscriber scan_sub;
   ros::Publisher obstacle_cluster_pub;
 
-  sensor_msgs::PointCloud obstacle_cluster;
+  sensor_msgs::PointCloud cluster;
 
-  //objects_info(x, y, k_dif, k_number, likelihood)
   MatrixXf objects_info;
-  MatrixXf cov;
-//  Vector1080d f;
-  Vector2f x;
-  Vector2f ave;
+  MatrixXf cluster_info;
+  VectorXf laser;
+  int obstacle_last_num;
 };
 
 int main(int argc, char **argv){
